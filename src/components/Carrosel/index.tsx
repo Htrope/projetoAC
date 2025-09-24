1918f2f336bb588f4318f4b576b2e18ca733a751
Htrope <tropehe12@gmail.com>
2025-09-23 22:36:19 -0300

    fix

--- src/components/Carrosel/index.tsx
+++ src/components/Carrosel/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import img1 from "../../assets/img1.jpg";
 import img3 from "../../assets/img3.jpg";
 import img4 from "../../assets/img4.jpg";
-import img5 from "../../assets/img5.jpg";
+import img5 from "../../assets/img5.jpg"
 
 const images = [img1, img3, img4, img5];
 
